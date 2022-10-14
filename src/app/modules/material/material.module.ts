f084d660731b6aef07bfbee3495fd3e44adeb5c2
kbskdev <milosz.olsztyn@gmail.com>
2022-10-14 17:32:51 +0200

    login form(prawie skonczony)

--- src/app/modules/material/material.module.ts
+++ src/app/modules/material/material.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import {MatGridListModule} from "@angular/material/grid-list";
 import {MatButtonModule} from "@angular/material/button";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatInputModule} from "@angular/material/input";
 
 
 
 @NgModule({
   declarations: [],
   exports:[MatGridListModule,
-            MatButtonModule]
+            MatButtonModule,
+          MatFormFieldModule,
+            MatInputModule,
+          ]
 })
 export class MaterialModule { }
