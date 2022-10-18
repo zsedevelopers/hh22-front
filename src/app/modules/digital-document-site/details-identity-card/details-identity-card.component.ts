475c1f2b482cfa1a7d71cf5daf58ee337414a259
Jakub Cendrowski <66736611+Quuba@users.noreply.github.com>
2022-10-18 19:27:20 +0200

    basic document details

--- src/app/modules/digital-document-site/details-identity-card/details-identity-card.component.ts
+++ src/app/modules/digital-document-site/details-identity-card/details-identity-card.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import IdentityCardDto from 'src/app/core/models/digital documents/identity-card-dto';
+import WalletDto from 'src/app/core/models/digital documents/wallet-dto';
+import { DigitalDocumentService } from 'src/app/core/services/digital-document.service';
 
 @Component({
   selector: 'app-details-identity-card',
   templateUrl: './details-identity-card.component.html',
-  styleUrls: ['./details-identity-card.component.scss']
+  styleUrls: ['./details-identity-card.component.scss'],
 })
 export class DetailsIdentityCardComponent implements OnInit {
+  identityCard: IdentityCardDto | null = null;
+  wallet: WalletDto | null = null;
 
-  constructor() { }
+  constructor(private documentService: DigitalDocumentService) {}
 
   ngOnInit(): void {
+    this.documentService.getWallet().subscribe((data) => {
+      this.wallet = data;
+      if (this.wallet != null) {
+        this.identityCard = this.wallet?.identityCard;
+      }
+    });
   }
-
 }
--- src/app/modules/digital-document-site/details-passport/details-passport.component.ts
+++ src/app/modules/digital-document-site/details-passport/details-passport.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import PassportDto from 'src/app/core/models/digital documents/passport-dto';
+import WalletDto from 'src/app/core/models/digital documents/wallet-dto';
+import { DigitalDocumentService } from 'src/app/core/services/digital-document.service';
 
 @Component({
   selector: 'app-details-passport',
   templateUrl: './details-passport.component.html',
-  styleUrls: ['./details-passport.component.scss']
+  styleUrls: ['./details-passport.component.scss'],
 })
 export class DetailsPassportComponent implements OnInit {
+  passport: PassportDto | null = null;
+  wallet: WalletDto | null = null;
 
-  constructor() { }
+  constructor(private documentService: DigitalDocumentService) {}
 
   ngOnInit(): void {
+    this.documentService.getWallet().subscribe((data) => {
+      this.wallet = data;
+      if (this.wallet != null) {
+        this.passport = this.wallet?.identityCard;
+      }
+    });
   }
-
 }
