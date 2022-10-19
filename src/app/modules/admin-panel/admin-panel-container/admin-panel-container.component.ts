c732f542f96a88e56b09f83ab3ce230d3a886388
Jakub Cendrowski <66736611+Quuba@users.noreply.github.com>
2022-10-19 18:33:51 +0200

    admin page authorization !don't forget to finish!

--- src/app/modules/admin-panel/admin-panel-container/admin-panel-container.component.ts
+++ src/app/modules/admin-panel/admin-panel-container/admin-panel-container.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { UserRole } from 'src/app/core/models/auth/UserRole';
+import UserDto from 'src/app/core/models/common/user-dto';
+import { AuthService } from 'src/app/core/services/auth.service';
 
 @Component({
   selector: 'app-admin-panel-container',
   templateUrl: './admin-panel-container.component.html',
-  styleUrls: ['./admin-panel-container.component.scss']
+  styleUrls: ['./admin-panel-container.component.scss'],
 })
 export class AdminPanelContainerComponent implements OnInit {
-
-  constructor() { }
+  constructor(private authService: AuthService, private router: Router) {}
 
   ngOnInit(): void {
-  }
+    //TODO: uncomment later !VERY IMPORTANT!
 
+    // if (!this.authService.isLogged()) {
+    //   console.warn('you have to be logged in to access this page')
+    //   this.router.navigate(['/']);
+    // } else {
+    //   this.authService.getUserData().subscribe((data: UserDto) => {
+    //     if (data == null || data.role != UserRole.ROLE_ADMIN) {
+    //       console.warn(`you're not an admin!`)
+    //       this.router.navigate(['/']);
+    //     }
+    //   });
+    // }
+  }
 }
