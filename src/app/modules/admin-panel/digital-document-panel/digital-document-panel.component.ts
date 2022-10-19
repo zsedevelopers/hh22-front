7cd884b96bb178525c036c3b67e256c3c3a3d48f
Jakub Cendrowski <66736611+Quuba@users.noreply.github.com>
2022-10-19 20:22:55 +0200

    html

--- src/app/modules/admin-panel/digital-document-panel/digital-document-panel.component.ts
+++ src/app/modules/admin-panel/digital-document-panel/digital-document-panel.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import DriverLicenceDto from 'src/app/core/models/digital documents/driver-licence-dto';
+import IdentityCardDto from 'src/app/core/models/digital documents/identity-card-dto';
+import PassportDto from 'src/app/core/models/digital documents/passport-dto';
+import { DigitalDocumentService } from 'src/app/core/services/digital-document.service';
 
 @Component({
   selector: 'app-digital-document-panel',
   templateUrl: './digital-document-panel.component.html',
-  styleUrls: ['./digital-document-panel.component.scss']
+  styleUrls: ['./digital-document-panel.component.scss'],
 })
 export class DigitalDocumentPanelComponent implements OnInit {
+  identityCards: IdentityCardDto[] = [];
+  passports: PassportDto[] = [];
+  drivingLicences: DriverLicenceDto[] = [];
 
-  constructor() { }
+  constructor(private documentService: DigitalDocumentService) {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
