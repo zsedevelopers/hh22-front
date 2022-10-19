91e5ad236c87b69be3e54da8bd3b368d04e793e3
Jakub Cendrowski <66736611+Quuba@users.noreply.github.com>
2022-10-19 12:09:14 +0200

    router fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { AuthComponent } from './modules/auth/auth.component';
 import { LoginComponent } from './modules/auth/pages/login/login.component';
 import { RegisterComponent } from './modules/auth/pages/register/register.component';
-import { ContainerComponent } from './modules/homepage/container/container.component';
-import { CivicProjectContainerComponent } from './modules/civic-project-site/civic-project-container/civic-project-container.component';
-import { AddCivicProjectComponent } from './modules/civic-project-site/add-civic-project/add-civic-project.component';
-import { ShowCivicProjectsComponent } from './modules/civic-project-site/show-civic-projects/show-civic-projects.component';
+
 import { DocumentContainerComponent } from './modules/digital-document-site/document-container/document-container.component';
 import { ShowDocumentsComponent } from './modules/digital-document-site/show-documents/show-documents.component';
 import { AddDocumentComponent } from './modules/digital-document-site/add-document/add-document.component';
@@ -17,8 +14,13 @@ import { DetailsContainerComponent } from './modules/digital-document-site/detai
 import { DetailsDriverLicenceComponent } from './modules/digital-document-site/details-driver-licence/details-driver-licence.component';
 import { DetailsIdentityCardComponent } from './modules/digital-document-site/details-identity-card/details-identity-card.component';
 import { DetailsPassportComponent } from './modules/digital-document-site/details-passport/details-passport.component';
+import { AddCivicProjectComponent } from './modules/civic-project-site/add-civic-project/add-civic-project.component';
+import { CivicProjectContainerComponent } from './modules/civic-project-site/civic-project-container/civic-project-container.component';
+import { ShowCivicProjectsComponent } from './modules/civic-project-site/show-civic-projects/show-civic-projects.component';
+import { MiddlebarComponent } from './modules/homepage/middlebar/middlebar.component';
+
 const routes: Routes = [
-  { path: '', component: ContainerComponent },
+  { path: '', component: MiddlebarComponent },
   {
     path: 'civicProject',
     component: CivicProjectContainerComponent,
@@ -61,24 +63,6 @@ const routes: Routes = [
     ],
   },
 ];
-import {CivicProjectContainerComponent} from "./modules/civic-project-site/civic-project-container/civic-project-container.component";
-import {AddCivicProjectComponent} from "./modules/civic-project-site/add-civic-project/add-civic-project.component";
-import {
-  ShowCivicProjectsComponent
-} from "./modules/civic-project-site/show-civic-projects/show-civic-projects.component";
-import {MiddlebarComponent} from "./modules/homepage/middlebar/middlebar.component";
-
-
-const routes: Routes = [
-    {path:'', component:MiddlebarComponent},
-    {path:'civicProject', component:CivicProjectContainerComponent, children:[
-        {path: 'add', component:AddCivicProjectComponent},
-        {path: 'show', component:ShowCivicProjectsComponent}]},
-
-  { path: 'auth', component: AuthComponent, children:[
-    {path: 'login', component:LoginComponent},
-    {path: 'register', component:RegisterComponent},
-]}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
