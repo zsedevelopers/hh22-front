0f2da9f2314a69e7d168ea306a5efd834a4238e6
Jakub Cendrowski <66736611+Quuba@users.noreply.github.com>
2022-10-19 18:33:51 +0200

    admin page authorization !don't forget to finish!

--- src/app/core/models/common/user-dto.ts
+++ src/app/core/models/common/user-dto.ts
@@ -1,9 +1,12 @@
-export default interface UserDto{
-    firstName:string;
-    secondName: string | null;
-    surname:string;
-    email:string;
-    phoneNumber:number;
-    pesel:string;
-    city:string;
+import { UserRole } from '../auth/UserRole';
+
+export default interface UserDto {
+  firstName: string;
+  secondName: string | null;
+  surname: string;
+  email: string;
+  phoneNumber: number;
+  pesel: string;
+  city: string;
+  role: UserRole;
 }
