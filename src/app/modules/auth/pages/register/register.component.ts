2dde1ad0eec1f9c81f84c70318e9d1b7244075e2
Jakub Cendrowski <66736611+Quuba@users.noreply.github.com>
2022-10-21 17:59:35 +0200

    partial register form validation

--- src/app/modules/auth/pages/register/register.component.ts
+++ src/app/modules/auth/pages/register/register.component.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import {
+  FormGroup,
+  FormControl,
+  Validators,
+  AbstractControl,
+  ValidationErrors,
+  ValidatorFn,
+} from '@angular/forms';
 import RegisterRequest from 'src/app/core/models/auth/register-request';
 import { AuthService } from 'src/app/core/services/auth.service';
 
@@ -13,20 +20,36 @@ export class RegisterComponent implements OnInit {
     firstName: new FormControl('', Validators.required),
     secondName: new FormControl(''),
     surname: new FormControl('', Validators.required),
-    email: new FormControl('', Validators.required),
+    email: new FormControl('', [Validators.required, Validators.email]),
     city: new FormControl('', Validators.required),
-    pesel: new FormControl('', Validators.required),
-    phoneNumber: new FormControl('', Validators.required),
+    pesel: new FormControl('', [
+      Validators.required,
+      Validators.pattern('[0-9]{11}'),
+    ]),
+    phoneNumber: new FormControl('', [
+      Validators.required,
+      // Validators.pattern("^(?:(?:(?:(?:\+|00)\d{2})?[ -]?(?:(?:\(0?\d{2}\))|(?:0?\d{2})))?[ -]?(?:\d{3}[- ]?\d{2}[- ]?\d{2}|\d{2}[- ]?\d{2}[- ]?\d{3}|\d{7})|(?:(?:(?:\+|00)\d{2})?[ -]?\d{3}[ -]?\d{3}[ -]?\d{3}))$"),
+    ]),
     password: new FormControl('', Validators.required),
     passwordConfirm: new FormControl('', Validators.required),
   });
+  errorMessage: string | null = null;
 
   constructor(private authService: AuthService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    // const regex: RegExp = new RegExp(
+    //   '^(?:(?:(?:(?:+|00)d{2})?[ -]?(?:(?:(0?d{2}))|(?:0?d{2})))?[ -]?(?:d{3}[- ]?d{2}[- ]?d{2}|d{2}[- ]?d{2}[- ]?d{3}|d{7})|(?:(?:(?:+|00)d{2})?[ -]?d{3}[ -]?d{3}[ -]?d{3}))$'
+    // );
+    // const res = regex.exec('asdasd')
+  }
   onRegisterFormSubmit() {
-    if (this.registerForm.invalid || !this.passwordsMatch()) {
+    if (this.registerForm.invalid) {
       console.warn('invalid form data');
+      return;
+    }
+    if (!this.passwordsMatch) {
+      this.errorMessage = 'Hasła muszą być identyczne';
       return;
     }
 
@@ -43,9 +66,15 @@ export class RegisterComponent implements OnInit {
     this.authService.register(requestData);
   }
 
-  private passwordsMatch() {
+  get passwordsMatch() {
+    console.log(this.registerForm.value.password);
+    console.log(this.registerForm.value.passwordConfirm);
+    console.log(
+      this.registerForm.value.password ==
+        this.registerForm.value.passwordConfirm
+    );
     return (
-      this.registerForm.value.password ===
+      this.registerForm.value.password ==
       this.registerForm.value.passwordConfirm
     );
   }
